d432a239775417928a1ada43feb11c36838b160f
Ricky Kim <rickykim78@gmail.com>
2019-06-03 12:22:38 +0100

    Update ec2-prep.sh

--- ec2-prep.sh
+++ ec2-prep.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Install Python3
 sudo yum -y install python36
+# Install PIP3
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py --user
+# Install required Python packages in user-specific directories
 pip3 install luigi --user
 pip3 install spotipy --user
 pip3 install boto3 --user
